978faeea2af59806d1a40c91cdfa6b67ca19b264
nikhilvatwani <nikhil.gymtrekker@gmail.com>
2022-04-12 00:18:49 -0400

    adding default import statement in temp.py

--- src/main/java/com/vizier/constants/VizierAgentConstants.java
+++ src/main/java/com/vizier/constants/VizierAgentConstants.java
@@ -6,4 +6,5 @@ public class VizierAgentConstants {
     public static String directoryPath = "."+ File.separator+"cell_content";
     public static String stubsDirectoryPath = directoryPath+File.separator+"stubs";
     public static String iniFilePath = directoryPath+File.separator+"mypy.ini";
+    public static String iniOriginalFilePath = "src"+File.separator+"main"+File.separator+"resources"+File.separator+"mypy.ini";
 }
--- src/main/java/com/vizier/directory/DirectoryInitializerImpl.java
+++ src/main/java/com/vizier/directory/DirectoryInitializerImpl.java
@@ -2,14 +2,14 @@ package com.vizier.directory;
 
 import com.vizier.stub.client.VizierStubServerClient;
 import com.vizier.stub.client.VizierStubServerClientImpl;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-import static com.vizier.constants.VizierAgentConstants.directoryPath;
-import static com.vizier.constants.VizierAgentConstants.iniFilePath;
+import static com.vizier.constants.VizierAgentConstants.*;
 
 public class DirectoryInitializerImpl implements DirectoryInitializer{
 
@@ -21,7 +21,10 @@ public class DirectoryInitializerImpl implements DirectoryInitializer{
 
     public boolean createIniFile() {
         try {
-            File myObj = new File(iniFilePath);
+            File copied = new File(iniFilePath);
+            File original = new File(iniOriginalFilePath);
+            FileUtils.copyFile(original, copied);
+            /*File myObj = new File(iniFilePath);
             if (myObj.createNewFile()) {
               System.out.println("File created: " + myObj.getName());
               FileWriter myWriter = new FileWriter(iniFilePath);
@@ -31,7 +34,7 @@ public class DirectoryInitializerImpl implements DirectoryInitializer{
 
             } else {
               System.out.println("File already exists.");
-            }
+            }*/
           } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -54,9 +57,12 @@ public class DirectoryInitializerImpl implements DirectoryInitializer{
     public boolean createTempFile(String filePath) {
         try {
             File myObj = new File(filePath);
-            
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
+                FileWriter myWriter = new FileWriter(filePath);
+                myWriter.write("from stubs.pycell.client import vizierdb #Do not delete this line");
+                myWriter.close();
+                System.out.println("Successfully wrote to the file.");
             } else {
                 System.out.println("File already exists.");
             }
