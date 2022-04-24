65a7472413b1192bd0cbe24d8f96e7b935734d19
nikhilvatwani <nikhil.gymtrekker@gmail.com>
2022-04-23 22:30:00 -0400

    changing mypy.ini file path

--- src/main/java/com/vizier/constants/VizierAgentConstants.java
+++ src/main/java/com/vizier/constants/VizierAgentConstants.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class VizierAgentConstants {
     public static String directoryPath = "."+ File.separator+"cell_content";
     public static String stubsDirectoryPath = directoryPath+File.separator+"stubs";
-    public static String iniFilePath1 = directoryPath+File.separator+"mypy.ini";
+    public static String iniFilePath1 = "mypy.ini";
     public static String stubsDirectory = "stubs";
     public static String iniFilePath = directoryPath+File.separator + "setup.cfg";
     public static String iniFile = "setup.cfg";
