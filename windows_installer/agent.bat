807f663130bd7f62dd770bb43a02b8ad5c5d358d
Your Name <MOTAMARRI ANVESH>
2022-04-26 21:54:09 -0400

    Windows Installer for maven wrapper

---
+++ windows_installer/agent.bat
@@ -0,0 +1,2 @@
+java -jar C:\Users\ANVESH\DOWNLO~1\VIZIER~1\VIZIER~1\WINDOW~2\..\target\vizier-agent2-0.0.1-SNAPSHOT.jar %1
+cmd /k
---
+++ windows_installer/install.bat
@@ -0,0 +1,14 @@
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+SET var=%cd%\..\agent.bat
+set var=%var:\=\\%
+powershell -Command "(gc test.reg) -replace 'VIZIERPATH', '%var%' | Out-File -encoding ASCII test.reg"
+
+SET vizierjar=%cd%\..\target\vizier-agent2-0.0.1-SNAPSHOT.jar
+powershell -Command "(gc ..\agent.bat) -replace 'VIZIERJARPATH', '%vizierjar%' | Out-File -encoding ASCII ..\agent.bat"
+
+reg import test.reg
+cd ..
+mvnw clean install
+
