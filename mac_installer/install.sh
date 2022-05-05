ccde62cc75c3705ef805804b409cc93eb339a82c
nikhilvatwani <nikhil.gymtrekker@gmail.com>
2022-05-05 17:59:04 -0400

    correcting mac installer

--- mac_installer/install.sh
+++ mac_installer/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 unzip -d ../mac_agent/ ../mac_agent/temp.app.zip
-echo 'export VIZIERPATH=$(pwd)/../target/vizier-agent2-0.0.1-SNAPSHOT.jar' >> ~/.zshenv
+currdir=$(pwd)
+echo 'export VIZIERPATH='$currdir'/../target/vizier-agent2-0.0.1-SNAPSHOT.jar' >> ~/.zshenv
 source ~/.zshenv
 
 chmod +x ../mvnw
