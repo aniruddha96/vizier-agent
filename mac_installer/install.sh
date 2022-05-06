8f49ff8d1356502a27bb29af4970403c092ad357
sakshi-mehra <sakshi.mehra@convosight.com>
2022-05-06 14:52:33 -0400

    Protocol fix

--- mac_installer/install.sh
+++ mac_installer/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 unzip -d ../mac_agent/ ../mac_agent/temp.app.zip
 currdir=$(pwd)
-echo 'export VIZIERPATH='$currdir'/../target/vizier-agent2-0.0.1-SNAPSHOT.jar' >> ~/.zshenv
+echo 'export NEWVIZIERPATH='$currdir'/../target/vizier-agent2-0.0.1-SNAPSHOT.jar' >> ~/.zshenv
 source ~/.zshenv
 
 chmod +x ../mvnw
