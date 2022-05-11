ab366f44ce83e9b1fe93df6238e40024739cc883
ANVESH841 <71516370+ANVESH841@users.noreply.github.com>
2022-05-11 00:06:06 -0400

    Update agent.bat

--- agent.bat
+++ agent.bat
@@ -1,5 +1,6 @@
 set var=%1
 set var=%var:x-vizier-client:=%
-cd /d VIZIERINVOKEDIR
-java -jar VIZIERJARPATH '%var%'
+cd /d D:\github\vizier-agent\windows_installer\..
+java -jar D:\github\vizier-agent\windows_installer\..\target\vizier-agent2-0.0.1-SNAPSHOT.jar %1
 cmd /k
+
